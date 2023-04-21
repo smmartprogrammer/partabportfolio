a23325b841a642ac47455e4df38569bebc550783
smmartprogrammer <partabrajp@gmail.com>
2023-04-22 02:58:15 +0500

    re gain its tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,65 @@
-import Image from 'next/image';
-import { ChatAlt2Icon, ChatAltIcon, ChatIcon } from '@heroicons/react/outline';
-
-import { Inter } from 'next/font/google';
+import Image from "next/image";
+import { ChatAltIcon } from "@heroicons/react/outline";
+import { SocialIcon } from "react-social-icons";
 
 export default function Home() {
   return (
     <main className="bg-[#2A2C39] w-screen">
-      <div className="flex justify-center items-center flex-wrap  ">
-        <div className="md:w-[50%] flex justify-center items-center  ">
-          <Image src="/partabb.png" alt="" width={300} height={100} />
-        </div>
-
-        <div className="md:w-[50%]  ">
-          <div className="text-4xl font-extrabold ">
-            <p>
-              Hi, I'm <span className="text-red-400">Partab!</span>{' '}
-            </p>
-            <p className="font-light">
-              I am developer, loves to solve problems
-            </p>
+      <div className="flex justify-center items-center flex-wrap-reverse">
+        <div className="md:w-[50%] flex mt-12">
+          <div className=" flex justify-center animate-pulse  ">
+            <div className=" flex flex-col items-center justify-center space-y-1 mx-6  ">
+              <SocialIcon
+                url="https://twitter.com/partabRparmar"
+                bgColor="transparent"
+                fgColor="white"
+                className="hover:text-red-400"
+              />
+              <SocialIcon
+                url="https://linkedin.com/in/jaketrent"
+                bgColor="transparent"
+                fgColor="white"
+              />
+              <SocialIcon
+                url="https://whatsapp.com/jaketrent"
+                bgColor="transparent"
+                fgColor="white"
+              />
+              <SocialIcon
+                url="https://gmail.com/jaketrent"
+                bgColor="transparent"
+                fgColor="white"
+              />
+            </div>
           </div>
-          <button className=" mt-5 flex items-center space-x-2   border-4 bg-[#252734] text-white border-[#797B87] animate-pulse hover:bg-red-400 text-white  px-4 rounded-md ">
-            <ChatAltIcon className="h-14 w-10 hover:animate-spin " />
-            <span className=" animate-bounce"> Connect Me.. </span>
-          </button>
-          <div className="flex space-x-4 mt-6 animate-bounce">
-            <img src="linkedin.png" width={30} height={30} alt="" />
-            <img src="insta.png" width={30} height={30} alt="" />
-            <img src="twiter.png" width={30} height={30} alt="" />
-            <img src="watsap.png" width={30} height={30} alt="" />
+          <div>
+            <div className="md:text-4xl font-extrabold  ">
+              <p>
+                Hi, I'm <span className="text-red-400">Partab!</span>
+              </p>
+              <p className="font-light">
+                I am developer, loves to solve problems
+              </p>
+            </div>
+            <button
+              className=" mt-5 flex items-center space-x-2
+             border-4 bg-[#252734] text-white border-[#797B87]
+              animate-pulse hover:bg-red-400 md:px-4 rounded-md  "
+            >
+              <ChatAltIcon className="md:h-14 md:w-10 hover:animate-spin " />
+              <span className="md:animate-bounce"> Connect Me.. </span>
+            </button>
+          </div>
+        </div>
+        <div className="md:w-[50%] flex justify-center items-center   ">
+          <div className="  flex justify-center items-center mt-6">
+            <Image
+              src="/partabaibackk.png"
+              alt=""
+              width={220}
+              height={220}
+              className="object-fit "
+            />
           </div>
         </div>
       </div>
